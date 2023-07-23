5376af387e7322601757b3839d9a638cc518fe8e
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-07-23 16:16:16 -0700

    Update gamefinalidea.py

--- gamefinalidea.py
+++ gamefinalidea.py
@@ -1,42 +1,59 @@
 import random
+
 class Character:
-    def __init__(self, name, health, attack_power):
+    def __init__(self, name, health, attack_power, mana):
         self.name = name
         self.health = health
         self.attack_power = attack_power
+        self.mana = mana 
+
+    def statsup(self, target, proc):
+        target.attack_power += 1
+        if target.attack_power >= 20 or proc == 1:
+            target.mana += 1
+            target.health += 2
 
     def attack(self, target):
         target.health -= self.attack_power
-        print(f"{self.name} attacks {target.name}. {target.name} now has {target.health} health.")
+        target.mana += 1
+        print(f"{self.name} attacks {target.name}. {target.name} now has {target.health} health and now has {target.mana} mana")
 
-    def heal(self,target):
-        target.health += self.attack_power/4
-        print(f"{self.name} attacks {target.name}. {target.name} now has {target.health} health.")
-        if target.health >= 50:
-            return
+    def heal(self, target):
+        if target.mana >= 2:
+            target.mana -= 2
+            target.health += self.attack_power / 4
+            if target.health >= 50:
+                return
+            print(f"{self.name} heals. {target.name} now has {target.health} health and {target.mana} mana left.")
+        else:
+            print('Not enough mana to heal')
 
-player = Character("Player", 50, 5)
-enemy = Character("Enemy", 30, 7)
+player = Character("Player", 50, 5, 0)
+enemy = Character("Enemy", 300, 7, 0)
 
 while player.health > 0 and enemy.health > 0:
-    action = input("Do you want to attack (yes/no) or heal? ").lower()
+    action = input("Do you want to attack, buff or heal? ").lower()
 
-    if action in ('yes','attack'):
+    if action == 'attack':
         player.attack(enemy)
         if enemy.health > 0:
             enemy.attack(player)
 
     elif action == 'heal':
-   
         if player.health == 50:
-            print('full hp')
+            print('You are already at full health!')
         else:
             player.heal(player)
-            print('player healed')
+
+    elif action == 'buff':
+        proc = random.randint(1, 3) 
+        player.statsup(player, proc)
+        print('Extra stats')
+        if proc == 1:
+            print("bonus stats")
 
     else:
         print("You decided not to attack.")
-    
 
 if player.health <= 0:
     print("You have been defeated!")
