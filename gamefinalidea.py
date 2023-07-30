2a439dcf0af3f715cfe8998d68393940a154f625
Zachary Jones <86993107+zachjones007@users.noreply.github.com>
2023-07-30 07:14:21 -0700

    Update gamefinalidea.py

--- gamefinalidea.py
+++ gamefinalidea.py
@@ -4,26 +4,32 @@
 import random
 
 class Character:
-    def __init__(self, name, health, attack_power, mana, bagspace=0, item_mapping=None):
+    def __init__(self, name, health, attack_power, mana, bagspace=0, item_mapping=None,cash = 0):
         self.name = name
         self.health = health
         self.attack_power = attack_power
         self.mana = mana 
         self.inventory = []
         self.bagspace = bagspace
+        self.cash = cash
+        self.item_mapping = item_mapping
         
         if item_mapping:
             self.item_mapping = item_mapping
         else:
-            self.item_mapping = {
-                1: {"name": 'HP potion lv1', "type": "item"},
-                2: {"name": 'Mana potion lv1', "type": "item"},
-                3: {"name": '10 gold coins', "type": "item"},
-                4: {"name": 'HP potion lv2', "type": "item"},
-                5: {"name": 'Mana potion lv2', "type": "item"},
-                6: {"name": '20 gold coins', "type": "potion"},
-                7: {"name": 'Magic beans', "type": "enemy"}
-            }
+            item_mapping = {
+    1: {"name": '10 gold coin', "type": "item"},
+    2: {"name": '20 gold coins', "type": "item"},
+    3: {"name": '30 gold coins', "type": "item"},
+    4: {"name": '40 gold coins', "type": "item"},
+    5: {"name": '50 gold coins', "type": "item"},
+    6: {"name": '60 gold coins', "type": "item"},
+    7: {"name": '70 gold coins', "type": "item"},
+    8: {"name": '80 gold coins', "type": "item"},
+    9: {"name": '90 gold coins', "type": "item"},
+    10: {"name": '100 gold coins', "type": "item"},
+    11: {"name": 'Magic beans', "type": "enemy"}
+        }
 
     def statsup(self, proc):
         self.attack_power += 1
@@ -51,15 +57,42 @@ class Character:
             print('Not enough mana to heal.')
 
     def Bag(self):
-        if self.bagspace >= 7:
+        if self.bagspace >= 10:
             print('Your bag is full!')
             return
 
-        item_number = random.randint(1, 7)
+        item_number = random.randint(1, 11)
+
         item_name = self.item_mapping[item_number]["name"]
         self.inventory.append(item_name)
-        self.bagspace += 1
+        if item_number == 11:
+            self.bagspace += 1
+        cashamount = (item_number*10)
+        if item_number <= 10:
+            self.cash += cashamount
         print(f"You found {item_name} and added it to your bag!")
+        itemstore = input('would you like to enter the shop?').lower()
+        if any(letter in itemstore for letter in ["y", "e", "s"]):
+            whichitem = int(input('would you like: hp potions(1), mana potions(2) or the magic beans(3)'))
+            
+            if whichitem == 1 and self.cash >= 35:
+                self.health += 10
+                self.cash -= 35
+                print(f"Thanks for buying the hp potion. You now have {self.health} health and {self.cash} cash.")
+            
+            elif whichitem == 2 and self.cash >= 25:
+                self.mana += 10
+                self.cash -= 25
+                print(f"Thanks for buying the mana potion. You now have {self.mana} mana and {self.cash} cash.")
+            
+            elif whichitem == 3 and self.cash >= 50:
+                self.inventory.append('Magic beans')
+                self.cash -= 50
+                print(f" magic beans coming right up. they are now your inventory! cash: {self.cash}")
+            
+            else:
+                print("You don't have enough cash for this item.")
+
 
     def separate_elements(self):
         items = []
@@ -104,6 +137,5 @@ while player.health > 0 and enemy.health > 0:
 if player.health <= 0:
     print("You have been defeated!")
 else:
-    print("You defeated the enemy!")
-
+    print("You defeated the enemy")
 
